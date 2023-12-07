bac24ada105458b4c7330858fab2b73201876848
Dan Hill <danshill@gmail.com>
2023-12-07 11:22:00 -0700

    initial commit

---
+++ static/hello.js
@@ -0,0 +1,14 @@
+console.log("Hello world from file")
+
+console.log("Displ info using console obj methods");
+const myObj = {
+    fruits: ["apples", "berries", "cherries"],
+    veggies: ["broccoli", "yams", "kale"]
+}
+console.table(myObj);
+
+// console.log(console);
+
+// console.dir(window);
+// console.log(window.location.href);
+
